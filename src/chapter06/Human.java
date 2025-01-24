b805a93f3f8cc4a7d69e40ea3c0a68cce88b1be5
ygk <vv566@naver.com>
2025-01-24 11:10:02 +0900

    Human 1.2V

--- src/chapter06/Human.java
+++ src/chapter06/Human.java
@@ -2,10 +2,34 @@ package chapter06;
 
 public class Human {
 
-    String name; // 이름
-    int height; // 신장
-    int weight; // 체중
-
+    private String name; // 이름
+    private int height; // 신장
+    private int weight; // 체중
 
     // 생성자
+    public Human( String name,  int height,  int weight ) {
+        this.name = name;
+        this.height =height;
+        this.weight = weight;
+    }
+
+    // getter
+    public String getName() {
+        return name;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public  int getWeight() {
+        return weight;
+    }
+    // 메서드
+    void gainWeight(int w) {
+        this.weight += weight;
+    }
+
+    void reduceWeight(int w) {
+        this.weight -= weight;
+    }
+
 }
--- src/chapter06/HumanTester.java
+++ src/chapter06/HumanTester.java
@@ -2,23 +2,20 @@ package chapter06;
 
 public class HumanTester {
     public static void main(String[] args) {
-        Human gildong = new Human();
-        Human chulsu = new Human();
+        Human gildong = new Human("홍길동", 170, 60);
+        Human chulsu = new Human("김철수", 166, 72);
 
-        gildong.name = "홍길동";
-        gildong.height = 170;
-        gildong.weight = 60;
+        gildong.gainWeight(3); // 홍길동이 3kg 쪘다
+        chulsu.reduceWeight(5); // 김철수가 5kg 빠졌다
 
-        chulsu.name = "김철수";
-        chulsu.height = 166;
-        chulsu.weight = 72;
 
-        System.out.println("이름:" + gildong.name);
-        System.out.println("신장:" + gildong.height + "cm");
-        System.out.println("체중:" + gildong.weight + "kg");
+        System.out.println("이름:" + gildong.getName());
+        System.out.println("신장:" + gildong.getHeight() + "cm");
+        System.out.println("체중:" + gildong.getWeight() + "kg");
         System.out.println();
-        System.out.println("이름:" + chulsu.name);
-        System.out.println("신장:" + chulsu.height + "cm");
-        System.out.println("체중:" + chulsu.weight + "kg");
+        System.out.println("이름:" + chulsu.getName());
+        System.out.println("신장:" + chulsu.getHeight() + "cm");
+        System.out.println("체중:" + chulsu.getWeight() + "kg");
     }
 }
+
